88a3afcedf773255226b85a5e2f8e95fd0afbc4c
leandro prates <leandro.prates@gmail.com>
2019-05-22 17:06:51 -0300

    proejto criacao pastas

---
+++ acordo-certo-test-domain/src/main/java/com/example/demo/model/Aluno.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo.model;
+
+import com.example.demo.ennum.Pais;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Data;
+
+/**
+ *
+ * @author lprates
+ */
+
+@Entity
+@Data
+public class Aluno {
+    
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)    
+    private Long id;
+    private String nome;
+    private String endereço;
+    @Enumerated(EnumType.STRING)
+    private Pais pais; 
+    private Boolean habilitado;
+    
+
+
+    
+}
---
+++ acordo-certo-test-service/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+import com.example.demo.ennum.Pais;
+import com.example.demo.model.Aluno;
+import com.example.demo.repository.AlunoRepository;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+        @Autowired
+        AlunoRepository alunoRepository ; 
+    
+    
+    
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+        @Override
+        public void run(String... args) throws Exception {
+            
+            Aluno aluno = new Aluno() ;
+            aluno.setEndereço("Avenida Joao Pessoa 408 ");
+            aluno.setHabilitado(Boolean.TRUE);
+            aluno.setNome("Leandro Prates");
+            aluno.setPais(Pais.BRASIL);
+            
+            alunoRepository.save(aluno) ; 
+            
+            Optional<Aluno> optional = alunoRepository.findById(1L);
+            if ( optional.isPresent() ) {
+                Aluno alunoRetorno = optional.get() ;
+                System.out.println("Saida Id Aluno : " + alunoRetorno.getId());
+            }
+            
+            
+        }
+
+}
